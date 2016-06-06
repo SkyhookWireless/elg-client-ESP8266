e46e746f269de1025c37f205fa7d7bc971557f87
Alexander Lin <alin@alin-mbp.local>
2016-06-06 16:58:12 -0400

    added handleScan and improved wifi connect

--- elg_client_demo/elg_client_demo.ino
+++ elg_client_demo/elg_client_demo.ino
@@ -6,6 +6,8 @@
 #include "FS.h"
 #include <ArduinoJson.h>
 
+// TODO: Allow the user to specify to restart in AP mode or Client Mode after AP change
+
 // provide text for the WiFi status
 const char *str_status[]= {
     "WL_IDLE_STATUS",
@@ -17,61 +19,77 @@ const char *str_status[]= {
     "WL_DISCONNECTED"
 };
 
+// Class allows WiFi to connect and check for connection 
+// status while also handling interrupts
 class WiFiWrapper{
-    bool connection_status;
+    // denotes if WiFi is currently connected
+    // last updated
     unsigned long previousMillis;
-    unsigned long interval;
-    int timeout;
     int check_times;
-    bool check_finish;
+    bool init_check_finish;
     
   public:
-  WiFiWrapper(unsigned long interval_input, int num_timeout){
-    connection_status = false;
-    interval = interval_input;
-    timeout = num_timeout;
+  WiFiWrapper(){
     check_times = 0;
-    bool check_finish = true;
+    bool init_check_finish = false;
   }
-  bool is_connected(){
-    return connection_status;
+  
+  bool get_init_check_finish(){
+    return init_check_finish;
   }
-  bool get_check_finish(){
-    return check_finish;
-  }
+
+  // run this function and then run init_check_connection
   void connect_to(String ssid, String password){
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
     WiFi.begin(ssid.c_str(),password.c_str());
-    check_finish = false;
-    connection_status = false;
+    init_check_finish = false;
     previousMillis = 0;
-    Serial.println("Attempting to Connect");
+    check_times = 0;
   }
-  void check_connection(){
+  
+  void connection_reset(){
+    WiFi.disconnect();
+    check_times = 0;
+    init_check_finish = false;
+  }
+
+  // wifimulti.run() for startup
+  bool check_connection(){
+    int n = WiFi.status();
+    // check if the WiFi is connected
+    if(n == WL_CONNECTED){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  void init_check_connection(unsigned long interval){
     unsigned long currentMillis = millis();
-    if(check_times < timeout){
       // if it is time to update
       if(currentMillis - previousMillis > interval){
         Serial.println("Checking Connection Status");
-        int n = WiFi.status();
-        // check if the WiFi is connected
-        if(n == WL_CONNECTED){
-          connection_status = true;
-          check_finish = true;
-          Serial.println("Connected");
-        }
-        // not yet connected check again later
-        else{
-          check_times++;
-        }
         
-        if (n > sizeof(str_status)) Serial.println(n);
-        else Serial.println(str_status[n]);
+        // check if the WiFi is connected
+        int n = WiFi.status();
+        switch(n){
+          case WL_NO_SSID_AVAIL:
+            init_check_finish = true;
+            break;
+          case WL_CONNECTED:
+            init_check_finish = true;
+            break;
+          case WL_CONNECT_FAILED:
+            init_check_finish = true;
+            break;
+          default:
+            check_times++;
+            break;
+        }
+        Serial.println(str_status[n]);
 
         // update the time
         previousMillis = currentMillis;
-      }
     }
   }
 };
@@ -81,7 +99,7 @@ const char *ssid = "ELG-Client";
 const char *password = "";
 
 ESP8266WebServer server(80);
-WiFiWrapper main_wifi(500,20);
+WiFiWrapper main_wifi;
 
 /* Just a little test message.  Go to http://192.168.4.1 in a web browser
  * connected to this access point to see it.
@@ -89,12 +107,14 @@ WiFiWrapper main_wifi(500,20);
 
 void setup() {
   SPIFFS.begin();
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.mode(WIFI_AP_STA);
   Serial.begin(115200);
   Serial.println();
   Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid, password);
-
+  WiFi.softAPmacAddress(mac);
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
@@ -123,7 +143,16 @@ void loop() {
 }
 
 void handleRoot() {
-  server.send(200, "text/html", "<head><link rel=\"stylesheet\" href=\"/bootstrap.min.css\"></link><script src=\"jquery.js\"></script></link><script src=\"bootstrap.min.js\"></script></head><h1>You are connected</h1><form action=\"/skyhookclient/changeap\" method=\"post\"><input type=\"text\" name=\"ssid\"><input type=\"password\" name=\"password\"><br><input type=\"submit\" value=\"Submit\"></form>");
+  // TODO scan surrounding networks
+  // TODO: Server send general HTML with requests for scan list instead of waiting for the scan
+  String connection = "";
+  if(WiFi.SSID() != NULL && WiFi.status() == WL_CONNECTED){
+    connection = "Connected To: " + WiFi.SSID();
+  }
+  else{
+    connection = "Not connected :(";
+  }
+  server.send(200, "text/html", "<head><link rel=\"stylesheet\" href=\"/bootstrap.min.css\"></link><script src=\"jquery.js\"></script></link><script src=\"bootstrap.min.js\"></script></head><h1>"+connection+"</h1><form action=\"/skyhookclient/changeap\" method=\"post\"><input type=\"text\" name=\"ssid\"><input type=\"password\" name=\"password\"><br><input type=\"submit\" value=\"Submit\"></form>");
 }
 
 void handleResources(){
@@ -132,8 +161,11 @@ void handleResources(){
   Serial.println("Requesting " + server.uri());
 
   File f;
-  if(SPIFFS.exists(server.uri()) && !isRestricted(server.uri())){
+  if(SPIFFS.exists(server.uri())){
     f = SPIFFS.open(server.uri(), "r");
+    if(!f){
+      Serial.println(server.uri() + " does not exist");
+    }
     if(server.streamFile(f, getDataType(server.uri())) != f.size()){
       Serial.println("Error");
     }
@@ -144,66 +176,110 @@ void handleResources(){
   }
 }
 
+// Returns a JSON of scanned networks
 void handleScan(){
-  server.send(204, "text/html", "<head><head><h1>No Content</h1>");
+  Serial.println("Scanning Networks");
+  int n = WiFi.scanNetworks();
+  DynamicJsonBuffer scan_obj_buf;
+  JsonObject& scan_obj = scan_obj_buf.createObject();
+  
+  for (int i = 0; i < n; i++){
+    DynamicJsonBuffer scan_info_buf;
+    JsonObject& scan_info = scan_obj.createNestedObject(WiFi.BSSIDstr(i));
+    scan_info["ssid"]=WiFi.SSID(i);
+    scan_info["channel"] = WiFi.channel(i);
+    scan_info["rssi"] = WiFi.RSSI(i);
+    scan_info["encrypt"] = encryptionTypeStr(WiFi.encryptionType(i));
+  }
+  scan_obj.prettyPrintTo(Serial);
+    size_t len = scan_obj.measurePrettyLength();
+  char* responseJSON = (char*)malloc(scan_obj.measurePrettyLength() * sizeof(char));
+  scan_obj.prettyPrintTo(responseJSON, scan_obj.measurePrettyLength());
+  server.send(200,"application/json",responseJSON);
 }
-
+// TODO: on client side, notify them that changing AP will disconnect them
 void handleChangeAP(){
   Serial.println("User is trying to change AP");
   if(server.hasArg("ssid") && server.hasArg("password")){
     Serial.println("SSID: " + server.arg("ssid"));
     Serial.println("Password: " + server.arg("password")); 
+    // server.send(200, "text/html", "<head></head><h1>Attemping to connect to SSID "+server.arg("ssid")+". AP will now shut down"+"</h1>");
   }
   
-  main_wifi.connect_to(server.arg("ssid"), server.arg("password"));
-  Serial.println("Attemtping to Connect");
-  Serial.println("Check Finish: " + main_wifi.get_check_finish());
-  while(!main_wifi.get_check_finish()){
-    main_wifi.check_connection();
+  // TODO: Tell user that the client will now disconnect and attempt to connect to the AP
+  // server send: restarting page
+  // OLED: Attempting to connect to SSID
+
+  //WIFI.begin() will move the AP channel 
+  main_wifi.connect_to(server.arg("ssid"), server.arg("pw"));
+  Serial.println("Current connection status: " + String(main_wifi.get_init_check_finish()));
+  while(!main_wifi.get_init_check_finish() && !main_wifi.check_connection()){
+    main_wifi.init_check_connection(500);
     delay(1);
     // Serial.println("Checking for interrupts");
     // Run Interrupt Class
   }
   Serial.println("Finished Checking for Wifi Connection");
-  if(main_wifi.is_connected()){
+  if(main_wifi.check_connection()){
     Serial.println("Connected to WiFi");
-    // save_connected_network(WiFi.BSSID(), WiFi.SSID(), server.arg("password"));
-
-  
-//
-//  object.prettyPrintTo(Serial);
-
-    
+    if(save_connected_network(server.arg("password"))){
+      print_saved_networks();
+      server.send(200,"text/html", "<head></head><h1>AP connected and saved</h1>");
+    }
+    // Send as internval service error if AP can't be saved
+    else{
+      Serial.println("Couldn't save AP to database");
+      //server.send(500,"text/html", "<head></head><h1>Couldn't save AP to database</h1>");
+    }
   }
+  // Device couldn't connect to AP 
   else{
     Serial.println("Failed to Connect");
+    //server.send(500, "text/html", "<head></head><h1>Failed to Connect</h1>");
   }
 }
 
+void print_saved_networks(){
+  char APjson[417];
+  if(SPIFFS.exists("/AP.json")){
+    File a = SPIFFS.open("/AP.json","r");
+    a.readBytesUntil(0,APjson,417);
+    a.close();
+  }
+  DynamicJsonBuffer bssid_obj_buf;
+  JsonObject& bssid_obj = bssid_obj_buf.parseObject(APjson);
+  bssid_obj.prettyPrintTo(Serial);
+}
+
+// Function will save the connected network into JSON formated file
+// TODO: Impose number of saved networks (currently magic numbers are
+// configured to three bssids
 bool save_connected_network(String password){
   // TODO: make configue file for magic numbers
   // 417 Because of max ssid and password calculation (will document)
   char APjson[417];
-  char* bssid = (char*)WiFi.BSSID();
-  
+  String bssid = WiFi.BSSIDstr();
+  Serial.println(bssid);
   // write existing data to a buffer
   if(SPIFFS.exists("/AP.json")){
     File a = SPIFFS.open("/AP.json","r");
     a.readBytesUntil(0,APjson,417);
     a.close();
   }
-  // if ssid/pw storage doesn't exist, make an empty json
+  // if ssid/pw storage doesn't exist, make an empty json array
   else{
     APjson[0] = '{';
     APjson[1] = '}';
   }
 
+  // dynamic buffers (should only be used for devices with larger RAM)
   DynamicJsonBuffer bssid_obj_buf;
   DynamicJsonBuffer bssid_info_buf;
 
   JsonObject& bssid_obj = bssid_obj_buf.parseObject(APjson);
   
-  // if the BSSID already exists we will update it by removing it and then readding it (keeping the stack style of BSSID storage)
+  // if the BSSID already exists we will update it by removing it and then 
+  // readding it (keeping the stack style of BSSID storage)
   if(bssid_obj[bssid] != NULL){
     bssid_obj.remove(bssid);
   }
@@ -217,25 +293,29 @@ bool save_connected_network(String password){
   }
   
   File b = SPIFFS.open("/APtmp.json","w+");
+  // ERROR: file couldn't be open or created
+  if(!b){
+    return 0;
+  }
   // write to file
   bssid_obj.printTo(b);
   b.close();
 
+  // delete the old AP list and update the new one to have the same name
   SPIFFS.remove("/AP.json");
   SPIFFS.rename("/APtmp.json", "/AP.json");
 
-  
-  // TODO: error checking not implemented yet
-  return 1;
+  // double checks that the JSON exists
+  if(SPIFFS.exists("/AP.json")){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
 
 void handleNotFound(){
   server.send(404, "text/html", "<head></head><h1>404 Not Found</h1>");
-}
-
-// TODO: Parse a config file on startup in order to make this dynamic 
-bool isRestricted(String path){
-  if(path=="/pw.txt" || path=="/ssid.txt") return true;
 }
 
 String getDataType(String path){
@@ -252,11 +332,19 @@ String getDataType(String path){
   else return "text/htm";
 }
 
-String bssidToString( uint8_t *bssid ) {
-
-  char mac[18] = {0};
-
-  sprintf( mac,"%02X:%02X:%02X:%02X:%02X:%02X", bssid[0],  bssid[1],  bssid[2], bssid[3], bssid[4], bssid[5] );
-  return String( mac );
-
+String encryptionTypeStr(uint8_t authmode) {
+    switch(authmode) {
+        case ENC_TYPE_NONE:
+            return "NONE";
+        case ENC_TYPE_WEP:
+            return "WEP";
+        case ENC_TYPE_TKIP:
+            return "TKIP";
+        case ENC_TYPE_CCMP:
+            return "CCMP";
+        case ENC_TYPE_AUTO:
+            return "AUTO";
+        default:
+            return "?";
+    }
 }
